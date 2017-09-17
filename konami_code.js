217bbb266e5751ad37d38f5d94a01af5ea76330c
Kayla Hansen <shiverr@gmail.com>
2017-09-17 18:36:21 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,8 +2,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let index = 0;
 
 function init() {
-  const body = document.querySelector('body')
-
+  const body = document.querySelector('body');
   body.addEventListener('keydown', function(e) {
     const key = parseInt(e.detail || e.which);
 
@@ -19,4 +18,3 @@ function init() {
    })
 }
 
-init()
